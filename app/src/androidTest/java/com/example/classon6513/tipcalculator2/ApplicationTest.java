9dd9194aa23fc5a27729eeec3a58788870a5a486
ClassonTech <dkc1998@gmail.com>
2016-02-12 11:08:45 -0600

    Old Code

---
+++ app/src/androidTest/java/com/example/classon6513/tipcalculator2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.classon6513.tipcalculator2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
